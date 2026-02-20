b81aaedbcd2c57fff16f731d1d582a68d10dc2a7
Sanndo Batengue <sanndobatengue@gmail.com>
2026-02-20 01:24:15 +0100

    Correction des pett incompatibiliter pour faire marcher le projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-
-
-
+import React from "react";
+import { useGameLogic } from "./hooks/useGameLogic";
 import QuestionCard from "./components/QuestionCard";
 import ScoreBoard from "./components/ScoreBoard";
 import ResultsScreen from "./components/ResultsScreen";
 
 function App() {
-
+ const {
+    currentQuestion,
+    currentIndex,
+    totalQuestions,
+    score,
+    gameOver,
+    selectedAnswer,
+    handleAnswer,
+    resetGame,
+  } = useGameLogic();
 
   return (
     <div style={{ fontFamily: "sans-serif", maxWidth: "700px", margin: "0 auto" }}>
       <h1 style={{ textAlign: "center", padding: "1.5rem", color: "#4f46e5" }}>
-        🧠 Quiz App
+        Quiz App
       </h1>
 
       {gameOver ? (
         <ResultsScreen
-
+          score={score}
+          totalQuestions={totalQuestions}
+          onRestart={resetGame}
         />
       ) : (
         <>
